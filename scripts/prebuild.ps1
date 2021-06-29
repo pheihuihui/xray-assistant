3f788b349c3b856846bb06e3795a5a81eaf469b5
Fei <33122873+pheihuihui@users.noreply.github.com>
2021-06-29 19:35:53 +0800

    ...

--- scripts/prebuild.ps1
+++ scripts/prebuild.ps1
@@ -11,10 +11,14 @@ proto-loader-gen-types `
     .\Xray-core\app\router\command\command.proto `
     .\Xray-core\app\stats\command\command.proto
 
-
 # get wsl ip
+Set-Location ..\
 $str = "export const ipaddr = '"
 $ip = (wsl hostname -I).trim()
-$str + $ip + "'" | Out-File -Encoding utf8 ..\\src\\ipaddr.ts
+$str + $ip + "'" | Out-File -Encoding utf8 .\src\ipaddr.ts
 
 # generate config.json for xray
+$jsonName = "xrayconfig.json"
+$userName = (wsl whoami)
+ts-node .\src\config.ts --files | Out-File -Encoding utf8 $jsonName
+echo "${userName}@${ip}:/home/${userName}"
